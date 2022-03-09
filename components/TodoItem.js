b0975a5c7c6f8acd6125f226e395adc37e1fe6ce
descholar-ceo <emmanuellamugi@gmail.com>
2022-03-10 00:10:05 +0200

    Add completed and delete actions btns

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import classNames from 'classnames'
 import React from 'react'
 import Button from './Button'
@@ -24,6 +25,20 @@ export default function TodoItem({ todo }) {
         'py-1',
         'rounded-md'
     );
+    const handleCompleted = async () => {
+        try {
+            await axios.patch(`${process.env.NEXT_PUBLIC_API}/todos/${todo.id}`, { completed: !todo.completed }, { headers: { 'Content-Type': 'application/json'}});
+        } catch (error) {
+            
+        }
+    }
+    const handleDelete = async () => {
+        try {
+            await axios.delete(`${process.env.NEXT_PUBLIC_API}/todos/${todo.id}`);
+        } catch (error) {
+            
+        }
+    }
   return (
     <div className={cardClasses}>
         <h1 className='text-center text-3xl text-blue-200 font-extrabold pb-5 pt-2'>{todo.name}</h1>
@@ -31,13 +46,13 @@ export default function TodoItem({ todo }) {
         <p className='text-sm'>{todo.time}</p>
         <footer className='flex justify-end p-3'>
             <span className='inline-block mx-2'>
-                <Button classes={statusBtnClass} text={todo.completed ? 'Completed' : 'Complete'} />
+                <Button handleClick={handleCompleted} classes={statusBtnClass} text={todo.completed ? 'Completed' : 'Complete'} />
             </span>
             <span className='inline-block mx-2'>
                 <Button classes='bg-blue-200 px-3 py-1 rounded-md' text='Edit' />
             </span>
             <span className='inline-block mx-2'>
-                <Button classes='bg-red-200 px-3 py-1 rounded-md' text='Delete' />
+                <Button handleClick={handleDelete} classes='bg-red-200 px-3 py-1 rounded-md' text='Delete' />
             </span>
         </footer>
     </div>
