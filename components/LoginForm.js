c732feb16f077bed8c247a1e2446b45c75d0d4c3
descholar-ceo <emmanuellamugi@gmail.com>
2022-03-11 08:06:36 +0200

    Add success login

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -3,27 +3,45 @@ import Button from './Button'
 import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form'
 import axios from 'axios';
+import { prepareDates } from '../utils/prepare-date';
+import classNames from 'classnames';
 
 export default function LoginForm() {
+    const [loginSuccess, setLoginSuccess] = useState('');
     const router = useRouter();
     const { register, handleSubmit } = useForm();
     const submitData = async (data) => {
         try {
         const gottenUser = await axios.get(`${process.env.NEXT_PUBLIC_API}/users?email=${data.email}&password=${data.password}`);
-        if (!!gottenUser && gottenUser.length !== 0) {
-            const toDay = new Date();
+        if (!!gottenUser && gottenUser.data && gottenUser.data.length !== 0) {
+            const { givenDay, nextDay } = prepareDates(new Date().getTime());
+            const data = { userId: gottenUser.data[0].id, createdAt: new Date(givenDay), expires: new Date(nextDay) };
+            console.log({ data });
             const newSession = await axios.post(
                 `${process.env.NEXT_PUBLIC_API}/sessions`,
-                { userId: gottenUser.id, createdAt: new Date(), epires: new Date() + 1});
+                data,
+                { headers: { 'Content-Type': 'application/json' } },
+            );
+            if (!!newSession && newSession.data) {
+                router.push('/');
+            } else {
+                return setLoginSuccess('fail');
+            }
+        } else {
+            return setLoginSuccess('fail');
         }
-        // router.push('/login');
         } catch (error) {
-        console.log({ error });
+            console.log({ error });
         }
     }
+    const loginErrorClass = classNames(
+        {'visible': loginSuccess === 'fail'},
+        {'invisible': loginSuccess === ''},
+    );
     return (
         <div>
-            <p className='text-red-600 my-5 bg-red-200 bg-opacity-50 mx-auto rounded-full text-ellipsis text-center text-lg'>Email or password not matching</p>
+            <p className={
+                `${loginErrorClass} text-red-600 my-5 bg-red-200 bg-opacity-50 mx-auto rounded-full text-ellipsis text-center text-lg`}>Email or password not matching</p>
             <form onSubmit={handleSubmit(submitData)} className='my-5 w-1/3 mx-auto border-2 p-2 rounded-lg border-slate-600 shadow-xl'>
             <div>
                 <input
--- utils/prepare-date.js
+++ utils/prepare-date.js
@@ -1,3 +1,16 @@
-export const prepareDates = (date) => {
-    
+export const prepareDates = (passedDate) => {
+    try {
+        const givenDate = new Date(passedDate);
+        const month = givenDate.getMonth() + 1;
+        const year = givenDate.getFullYear();
+        const date = givenDate.getDate(); // passed date
+        const nextDate = givenDate.getDate() + 1; // next day
+
+        return {
+            givenDay: `${year}-${month}-${date}`,
+            nextDay: `${year}-${month}-${nextDate}`,
+        }
+    } catch (error) {
+        console.log({ error });
+    }
 }
